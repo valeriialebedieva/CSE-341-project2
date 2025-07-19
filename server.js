8669758707fa4bc0492547fcaf83bafa3687d89a
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-07-19 16:25:16 +0200

    add w3 task

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 const app = express();
 const mongodb = require('./data/db');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-})
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitiated: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', require('./routers'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
+app.use(cors({methods:['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}));
+app.use(cors({origin: '*'}));
+app.use('/', require('./routes/index.js'));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACKURL
+},
+function(accessToken, refreshToken, profile, done){
+  return done(null, profile);
+}
+));
 
 mongodb.initDb((err) => {
   if (err) {
