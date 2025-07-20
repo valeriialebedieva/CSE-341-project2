e17c75efa0d0d2d746fdef5588766adbe7f0158f
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-07-20 15:37:13 +0200

    adde w3 task

--- routers/books.js
+++ routers/books.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { isAuthenticated } = require('../middleware/authenticate');
 const booksController = require('../controllers/books');
 
 router.get('/', booksController.getAllBooks);
 router.get('/:id', booksController.getBook);
-router.post('/', booksController.createBook);
-router.put('/:id', booksController.updateBook);
-router.delete('/:id', booksController.deleteBook);
+router.post('/', isAuthenticated, booksController.createBook);
+router.put('/:id', isAuthenticated, booksController.updateBook);
+router.delete('/:id', isAuthenticated, booksController.deleteBook);
 
 module.exports = router;
